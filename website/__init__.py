5d1d3a4cd75d6df6fb7deacabbd9f31f29bd1eb7
chetanloharpop <https://github.com>
2021-04-28 23:50:34 +0530

    change10s

--- website/__init__.py
+++ website/__init__.py
@@ -12,7 +12,7 @@ from os import path
 def create_app():
    
     app = Flask(__name__)
-  #  run_with_ngrok(app)
+    #run_with_ngrok(app)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
    # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
   #  db.init_app(app)
--- website/views.py
+++ website/views.py
@@ -17,8 +17,9 @@ from alice_blue import *
 import json
 import math
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+import datetime
 import pypyodbc
+import ast
 #access_token = AliceBlue.login_and_get_access_token(username='AB169563', password='sharu12311', twoFA='a',  api_secret='K0VSBC2Y6NNZ6QFI9BVQK4VIYR4UW07DX60BV2RFESA6ECRX1C581UWQFM4UIHBG')
 #alice = AliceBlue(username='AB169563', password='sharu12311', access_token=access_token)
 headers = {
@@ -43,19 +44,33 @@ def logic():
     #json_data = alice.get_balance() # get balance / margin limits
     #margin_avaiable = json_data['data']['cash_positions'][1]['available']['cashmarginavailable']
     margin_avaiable = 100000
-    order = request.get_data();
-    x = order.split()
-    
-    if 'entry' == 'entry':
-        qty = int(float(margin_avaiable * 0.02)) / int(float(x[1]))
-        print('qty',math.floor(qty))
-        valid = '1'
-        oms_order_id='123'
-        cursor.execute("INSERT INTO stocksks(stock_name, price, st, mode, status,valid,orderid,dat) VALUES(?,?,?,?,?,?,?,CURRENT_TIMESTAMP) ", (x[0],x[1],x[2],x[3],x[4],valid,oms_order_id))   
-        connection.commit()
+    output = str(request.get_data(as_text=True));
+    data = ast.literal_eval(output)
+    print(data)
+    #order = request.get_data();
+    #x = order.split()
+    #GSM 888 123 long exit
+    if data['status'] == 'entry':
+        sql ="select COUNT(*) from stocksks where stock_name = ? and status= ? and valid=1"
+        adr = (data['stock_name'], 'entry')
+        op = cursor.execute(sql,adr)
+        #op = cursor.execute(sql)
+        row1= cursor.fetchone()[0]
+        print('row1')
+        print(row1)
+        if row1 == 0:
+            print('inserting data')
+            qty = int(float(margin_avaiable * 0.02)) / int(float(data['price']))
+            print('qty',math.floor(qty))
+            valid = '1'
+            oms_order_id='123'
+            currentdate = datetime.datetime.now()
+            print(currentdate)
+            cursor.execute("INSERT INTO stocksks(stock_name, price, st, mode, status,valid,orderid,dat) VALUES(?,?,?,?,?,?,?,?) ", (data['stock_name'],data['price'],data['st'],data['mode'],data['status'],valid,oms_order_id,currentdate))   
+            connection.commit()
         
         
-        '''if qty >0 and x[3]=='long':
+        '''if qty >0 and data['mode']=='long':
             response = alice.place_order(transaction_type = TransactionType.Buy,
                          instrument = alice.get_instrument_by_symbol('NSE', 'INFY'),
                          quantity = qty,
@@ -63,12 +78,12 @@ def logic():
                          product_type = ProductType.Intraday,
                          #price = 0.0,
                          #trigger_price = 8.0,
-                         stop_loss = x[2],
+                         stop_loss = data['st'],
                          square_off = None,
                          trailing_sl = None,
                          is_amo = False)
            
-        if qty >0 and x[3]=='short':
+        if qty >0 and data['mode']=='short':
             response = alice.place_order(transaction_type = TransactionType.Sell,
                          instrument = alice.get_instrument_by_symbol('NSE', 'INFY'),
                          quantity = qty,
@@ -76,36 +91,56 @@ def logic():
                          product_type = ProductType.Intraday,
                          #price = 0.0,
                          #trigger_price = 8.0,
-                         stop_loss = x[2],
+                         stop_loss = data['st'],
                          square_off = None,
                          trailing_sl = None,
                          is_amo = False)     
             
         if((response["status"]=="success") and (response["message"]=="Order placed successfully")):
             oms_order_id = response["data"]["oms_order_id"]
-            cursor.execute("INSERT INTO stocksks(stock_name, price, st, mode, status,valid,orderid,dat) VALUES(?,?,?,?,?,?,?,CURRENT_TIMESTAMP) ", (x[0],x[1],x[2],x[3],x[4],valid,oms_order_id))
+            cursor.execute("INSERT INTO stocksks(stock_name, price, st, mode, status,valid,orderid,dat) VALUES(?,?,?,?,?,?,?,?) ", (data['stock_name'],data['price'],data['st'],data['mode'],data['status'],valid,oms_order_id,currentdate))
             connection.commit()
-            msg=x[0]+' buyed at '+x[1]
+            msg=data['stock_name']+' '+data['mode'] + ' at price '+ data['price']
             payload = "sender_id=FSTSMS&message="+msg+"&language=english&route=p&numbers=8308831893"
             response1 = requests.request("POST", url, data=payload, headers=headers)
             '''
 
-    if 'exit11' == 'exit':
-        order = request.get_data();
-        x = order.split()
+    if data['status'] == 'exit':
+        print('stock name')
+        print(data['stock_name'])
+        #order = request.get_data();
+        #x = order.split()
         #change the GSM valoe to x[0] stock name
-        sql ="""select orderid from stocksks where stock_name = %s and status= %s""" %("'GSM'","'entry'")
+        #sql ="""select orderid from stocksks where stock_name = %s and status= %s""" %(data['stock_name'],"'entry'")
         
-        cursor.execute(sql)
-        row = cursor.fetchone()[0]
-        print(row)
-        #response = alice.cancel_order(oid) #Cancel an open order     
-       # if((response["status"]=="success")):
-        if(("success"=="success")):
-            cursor.execute("INSERT INTO stocksks (stock_name, price, st, mode, status,orderid,valid,dat) VALUES (?,?,?,?,?,?,?,CURRENT_TIMESTAMP)",(x[0],x[1],x[2],x[3],x[4],row,0))
-            connection.commit()
-            updatesql = """update stocksks set valid= %s where orderid= %s""" %(0,row)
-            cursor.execute(updatesql)
-            connection.commit()
+        
+        sql ="select COUNT(*) from stocksks where stock_name = ? and status= ? and valid=1"
+        adr = (data['stock_name'], 'entry')
+        op = cursor.execute(sql,adr)
+        #op = cursor.execute(sql)
+        row2= cursor.fetchone()[0]
+        print('row2')
+        print(row2)
+        
+        if row2 > 0:
+            print("closing trade")
+            sql ="select orderid from stocksks where stock_name = ? and status= ? and valid=1"
+            adr = (data['stock_name'], 'entry')
+            op = cursor.execute(sql,adr)
+            row3 = cursor.fetchone()[0]
+            print(row3)
+            currentdate1 = datetime.datetime.now()
+            
+            #response = alice.cancel_order(oid) #Cancel an open order     
+           # if((response["status"]=="success")):
+            if(("success"=="success")):
+                cursor.execute("INSERT INTO stocksks (stock_name, price, st, mode, status,orderid,valid,dat) VALUES (?,?,?,?,?,?,?,?)",(data['stock_name'],data['price'],data['st'],data['mode'],data['status'],row3,0,currentdate1))
+                connection.commit()
+                updatesql = """update stocksks set valid= %s where orderid= %s""" %(0,row3)
+                cursor.execute(updatesql)
+                connection.commit()
+                #msg=data['stock_name']+' '+data['mode'] + ' at price '+ data['price']
+                #payload = "sender_id=FSTSMS&message="+msg+"&language=english&route=p&numbers=8308831893"
+                #response1 = requests.request("POST", url, data=payload, headers=headers)
 
     return "hello chetan"
